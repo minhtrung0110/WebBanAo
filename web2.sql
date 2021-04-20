ebb01f67e0db8b98730fe10b893b5a8bc36363d8
unknown <nguyenducminhtrung0044@gmail.com>
2021-04-20 14:02:25 +0700

    Update DB

--- web2.sql
+++ web2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3307
--- Generation Time: Apr 12, 2021 at 05:27 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.2
+-- Host: 127.0.0.1
+-- Generation Time: Apr 20, 2021 at 09:00 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `web2`
+-- Database: `doanweb2`
 --
 
 -- --------------------------------------------------------
@@ -54,11 +54,55 @@ CREATE TABLE `chitiethoadon` (
 --
 
 CREATE TABLE `chitietphieunhap` (
-  `MA_CTPN` int(10) UNSIGNED NOT NULL,
-  `MA_SP` int(10) UNSIGNED DEFAULT NULL,
-  `DON_GIA` float NOT NULL,
-  `SO_LUONG` int(11) NOT NULL
+  `MA_PN` int(10) UNSIGNED NOT NULL,
+  `MA_SP` int(10) UNSIGNED NOT NULL,
+  `DON_GIA` int(11) NOT NULL,
+  `SIZE` varchar(2) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `SO_LUUONG` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+--
+-- Dumping data for table `chitietphieunhap`
+--
+
+INSERT INTO `chitietphieunhap` (`MA_PN`, `MA_SP`, `DON_GIA`, `SIZE`, `SO_LUUONG`) VALUES
+(1, 1, 450000, 'S', 40),
+(1, 2, 450000, 'M', 40),
+(1, 3, 450000, 'L', 40),
+(1, 4, 450000, 'S', 50),
+(1, 5, 450000, 'M', 100),
+(1, 6, 450000, 'L', 80),
+(1, 7, 450000, 'S', 40),
+(1, 8, 450000, 'M', 40),
+(1, 9, 450000, 'L', 40),
+(1, 10, 450000, 'S', 80),
+(1, 11, 450000, 'M', 80),
+(1, 12, 450000, 'L', 80),
+(2, 13, 450000, 'S', 100),
+(2, 14, 450000, 'M', 100),
+(2, 15, 450000, 'L', 100),
+(2, 16, 450000, 'S', 100),
+(2, 17, 450000, 'M', 100),
+(2, 18, 450000, 'L', 100),
+(2, 19, 450000, 'S', 100),
+(2, 20, 450000, 'M', 100),
+(2, 21, 450000, 'L', 100),
+(2, 13, 450000, 'S', 100),
+(2, 14, 450000, 'M', 100),
+(2, 15, 450000, 'L', 100),
+(2, 16, 450000, 'S', 100),
+(2, 17, 450000, 'M', 100),
+(2, 18, 450000, 'L', 100),
+(2, 19, 450000, 'S', 100),
+(2, 20, 450000, 'M', 100),
+(2, 21, 450000, 'L', 100),
+(2, 22, 450000, 'S', 100),
+(2, 23, 450000, 'M', 100),
+(2, 24, 450000, 'L', 100),
+(2, 22, 450000, 'S', 100),
+(2, 23, 450000, 'M', 100),
+(2, 24, 450000, 'L', 100),
+(2, 22, 450000, 'S', 100);
 
 -- --------------------------------------------------------
 
@@ -110,7 +154,9 @@ CREATE TABLE `groupquyen` (
 --
 
 INSERT INTO `groupquyen` (`MA_GROUP_QUYEN`, `TEN_GROUP_QUYEN`) VALUES
-(1, 'admin');
+(1, 'admin'),
+(2, 'nhanvien'),
+(3, 'khachhang');
 
 -- --------------------------------------------------------
 
@@ -170,11 +216,18 @@ CREATE TABLE `nhanvien` (
 
 CREATE TABLE `phieunhap` (
   `MA_PN` int(10) UNSIGNED NOT NULL,
-  `MA_CTPN` int(10) UNSIGNED DEFAULT NULL,
-  `MA_TK` int(10) UNSIGNED DEFAULT NULL,
-  `NGAY_NHAP` datetime NOT NULL,
+  `MA_TK` int(10) UNSIGNED NOT NULL,
+  `NGAY_NHAP` date NOT NULL,
   `TONG_DON_GIA` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+--
+-- Dumping data for table `phieunhap`
+--
+
+INSERT INTO `phieunhap` (`MA_PN`, `MA_TK`, `NGAY_NHAP`, `TONG_DON_GIA`) VALUES
+(1, 1, '2021-03-01', 10000000),
+(2, 1, '2021-03-31', 20000000);
 
 -- --------------------------------------------------------
 
@@ -212,6 +265,60 @@ CREATE TABLE `sanpham` (
   `HINH_ANH_URL` text COLLATE utf8mb4_vietnamese_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
 
+--
+-- Dumping data for table `sanpham`
+--
+
+INSERT INTO `sanpham` (`MA_SP`, `TEN_SP`, `SO_LUONG`, `DON_GIA`, `LOAI_SP`, `KICH_THUOC`, `MO_TA`, `HINH_ANH_URL`) VALUES
+(1, 'Tee HappyAniversary Yellow', 40, 500000, 'Tee', 'S', 'Kỷ Niệm 3 năm thành lập Three Clothing', 'aniversary01.jpg'),
+(2, 'Tee HappyAniversary Yellow', 40, 500000, 'Tee', 'M', 'Kỷ Niệm 3 năm thành lập Three Clothing', 'aniversary01.jpg'),
+(3, 'Tee HappyAniversary Yellow', 40, 500000, 'Tee', 'L', 'Kỷ Niệm 3 năm thành lập Three Clothing', 'aniversary01.jpg'),
+(4, 'Aniversary Big Logo', 50, 500000, 'Tee', 'S', 'Kỷ Niệm 3 năm thành lập Three Clothing', 'aniversary02.jpg'),
+(5, 'Aniversary Big Logo', 100, 500000, 'Tee', 'M', 'Kỷ Niệm 3 năm thành lập Three Clothing', 'aniversary02.jpg'),
+(6, 'Aniversary Big Logo', 80, 500000, 'Tee', 'L', 'Kỷ Niệm 3 năm thành lập Three Clothing', 'aniversary02.jpg'),
+(7, 'HappyAniversary Special Fire', 40, 600000, 'Tee', 'S', 'Kỷ Niệm 3 năm thành lập Three Clothing', 'aniversary03.jpg'),
+(8, 'HappyAniversary Special Fire', 40, 600000, 'Tee', 'M', 'Kỷ Niệm 3 năm thành lập Three Clothing', 'aniversary03.jpg'),
+(9, 'HappyAniversary Special Fire', 40, 600000, 'Tee', 'L', 'Kỷ Niệm 3 năm thành lập Three Clothing', 'aniversary03.jpg'),
+(10, 'HappyAniversary Church', 80, 500000, 'Tee', 'S', 'Kỷ Niệm 3 năm thành lập Three Clothing', 'aniversary04.jpg'),
+(11, 'HappyAniversary Church', 80, 500000, 'Tee', 'M', 'Kỷ Niệm 3 năm thành lập Three Clothing', 'aniversary04.jpg'),
+(12, 'HappyAniversary Church', 80, 500000, 'Tee', 'L', 'Kỷ Niệm 3 năm thành lập Three Clothing', 'aniversary04.jpg'),
+(13, 'BIG-LOGO-LVENTS', 100, 550000, 'Tee', 'S', 'Không Mua Bây Giờ Thì Mua Bao Giờ!!!', 'BIG-LOGO-LVENTS.jpg'),
+(14, 'BIG-LOGO-LVENTS', 100, 550000, 'Tee', 'M', 'Không Mua Bây Giờ Thì Mua Bao Giờ!!!', 'BIG-LOGO-LVENTS.jpg'),
+(15, 'BIG-LOGO-LVENTS', 100, 550000, 'Tee', 'L', 'Không Mua Bây Giờ Thì Mua Bao Giờ!!!', 'BIG-LOGO-LVENTS.jpg'),
+(16, 'BIG-LOGO-LVENTS', 100, 550000, 'Tee', 'S', 'Không Mua Bây Giờ Thì Mua Bao Giờ!!!', 'BIG-LOGO-LVENTS.jpg'),
+(17, 'BIG-LOGO-LVENTS', 100, 550000, 'Tee', 'M', 'Không Mua Bây Giờ Thì Mua Bao Giờ!!!', 'BIG-LOGO-LVENTS.jpg'),
+(18, 'BIG-LOGO-LVENTS', 100, 550000, 'Tee', 'L', 'Không Mua Bây Giờ Thì Mua Bao Giờ!!!', 'BIG-LOGO-LVENTS.jpg'),
+(19, 'HOLLIDAY SUMMER', 100, 580000, 'Tee', 'S', 'Không Mua Bây Giờ Thì Mua Bao Giờ!!!', 'Holiday_Summer.jpg'),
+(20, 'HOLLIDAY SUMMER', 100, 580000, 'Tee', 'M', 'Không Mua Bây Giờ Thì Mua Bao Giờ!!!', 'Holiday_Summer.jpg'),
+(21, 'HOLLIDAY SUMMER', 100, 580000, 'Tee', 'L', 'Không Mua Bây Giờ Thì Mua Bao Giờ!!!', 'Holiday_Summer.jpg'),
+(22, 'HOLLIDAY FUNNY', 100, 600000, 'Tee', 'S', 'Không Mua Bây Giờ Thì Mua Bao Giờ!!!', 'Holiday_Summer-Fun.jpg'),
+(23, 'HOLLIDAY FUNNY', 100, 600000, 'Tee', 'M', 'Không Mua Bây Giờ Thì Mua Bao Giờ!!!', 'Holiday_Summer-Fun.jpg'),
+(24, 'HOLLIDAY FUNNY', 100, 600000, 'Tee', 'L', 'Không Mua Bây Giờ Thì Mua Bao Giờ!!!', 'Holiday_Summer-Fun.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `slider`
+--
+
+CREATE TABLE `slider` (
+  `ID` int(11) NOT NULL,
+  `src` varchar(50) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `active` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+--
+-- Dumping data for table `slider`
+--
+
+INSERT INTO `slider` (`ID`, `src`, `active`) VALUES
+(1, 'img1.jpg', 1),
+(2, 'img2.jpg', 1),
+(3, 'img3.jpg', 1),
+(4, 'img4.jpg', 1),
+(5, 'img5.jpg', 1),
+(6, 'img6.jpg', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -222,16 +329,21 @@ CREATE TABLE `taikhoan` (
   `MA_TK` int(10) UNSIGNED NOT NULL,
   `MA_GROUP_QUYEN` int(10) UNSIGNED DEFAULT NULL,
   `TEN_DANG_NHAP` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_vietnamese_ci NOT NULL,
-  `MAT_KHAU` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_vietnamese_ci NOT NULL,
-  `STATUS` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_vietnamese_ci NOT NULL
+  `MAT_KHAU` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `STATUS` int(1) NOT NULL DEFAULT 1,
+  `EMAIL` varchar(50) COLLATE utf8_vietnamese_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
 -- Dumping data for table `taikhoan`
 --
 
-INSERT INTO `taikhoan` (`MA_TK`, `MA_GROUP_QUYEN`, `TEN_DANG_NHAP`, `MAT_KHAU`, `STATUS`) VALUES
-(1, 1, 'admin', 'admin', '1');
+INSERT INTO `taikhoan` (`MA_TK`, `MA_GROUP_QUYEN`, `TEN_DANG_NHAP`, `MAT_KHAU`, `STATUS`, `EMAIL`) VALUES
+(0, NULL, 'Thuan', 'e10adc3949ba59abbe56e057f20f883e', 1, 'thuan@gmail.com'),
+(1, 1, 'admin', 'admin', 1, 'admin@gmail.com'),
+(2, 2, 'Vy', '123456', 1, 'chautuongvy@gmail.com'),
+(3, NULL, 'Đại Nam', 'e10adc3949ba59abbe56e057f20f883e', 1, 'dai123@gmail.com'),
+(8, NULL, 'Minh Trung', 'e10adc3949ba59abbe56e057f20f883e', 1, 'minhtrung@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -255,7 +367,7 @@ ALTER TABLE `chitiethoadon`
 -- Indexes for table `chitietphieunhap`
 --
 ALTER TABLE `chitietphieunhap`
-  ADD PRIMARY KEY (`MA_CTPN`),
+  ADD KEY `MA_PN` (`MA_PN`),
   ADD KEY `MA_SP` (`MA_SP`);
 
 --
@@ -305,7 +417,6 @@ ALTER TABLE `nhanvien`
 --
 ALTER TABLE `phieunhap`
   ADD PRIMARY KEY (`MA_PN`),
-  ADD KEY `MA_CTPN` (`MA_CTPN`),
   ADD KEY `MA_TK` (`MA_TK`);
 
 --
@@ -339,12 +450,6 @@ ALTER TABLE `chitiethoadon`
   MODIFY `MA_CTHD` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `chitietphieunhap`
---
-ALTER TABLE `chitietphieunhap`
-  MODIFY `MA_CTPN` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `chuongtrinhgiamgia`
 --
 ALTER TABLE `chuongtrinhgiamgia`
@@ -360,7 +465,7 @@ ALTER TABLE `danhmuc`
 -- AUTO_INCREMENT for table `groupquyen`
 --
 ALTER TABLE `groupquyen`
-  MODIFY `MA_GROUP_QUYEN` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `MA_GROUP_QUYEN` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `hoadon`
@@ -384,13 +489,13 @@ ALTER TABLE `nhanvien`
 -- AUTO_INCREMENT for table `phieunhap`
 --
 ALTER TABLE `phieunhap`
-  MODIFY `MA_PN` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `MA_PN` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `sanpham`
 --
 ALTER TABLE `sanpham`
-  MODIFY `MA_SP` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `MA_SP` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- Constraints for dumped tables
@@ -400,8 +505,8 @@ ALTER TABLE `sanpham`
 -- Constraints for table `chitietgiamgia`
 --
 ALTER TABLE `chitietgiamgia`
-  ADD CONSTRAINT `chitietgiamgia_ibfk_1` FOREIGN KEY (`MA_SP`) REFERENCES `sanpham` (`MA_SP`),
-  ADD CONSTRAINT `chitietgiamgia_ibfk_2` FOREIGN KEY (`MA_SP`) REFERENCES `chuongtrinhgiamgia` (`MA_CTGG`);
+  ADD CONSTRAINT `chitietgiamgia_ibfk_2` FOREIGN KEY (`MA_SP`) REFERENCES `chuongtrinhgiamgia` (`MA_CTGG`),
+  ADD CONSTRAINT `chitietgiamgia_ibfk_3` FOREIGN KEY (`MA_SP`) REFERENCES `sanpham` (`MA_SP`);
 
 --
 -- Constraints for table `chitiethoadon`
@@ -413,7 +518,8 @@ ALTER TABLE `chitiethoadon`
 -- Constraints for table `chitietphieunhap`
 --
 ALTER TABLE `chitietphieunhap`
-  ADD CONSTRAINT `chitietphieunhap_ibfk_1` FOREIGN KEY (`MA_SP`) REFERENCES `sanpham` (`MA_SP`);
+  ADD CONSTRAINT `chitietphieunhap_ibfk_1` FOREIGN KEY (`MA_PN`) REFERENCES `phieunhap` (`MA_PN`),
+  ADD CONSTRAINT `chitietphieunhap_ibfk_2` FOREIGN KEY (`MA_SP`) REFERENCES `sanpham` (`MA_SP`);
 
 --
 -- Constraints for table `hoadon`
@@ -440,8 +546,7 @@ ALTER TABLE `nhanvien`
 -- Constraints for table `phieunhap`
 --
 ALTER TABLE `phieunhap`
-  ADD CONSTRAINT `phieunhap_ibfk_1` FOREIGN KEY (`MA_CTPN`) REFERENCES `chitietphieunhap` (`MA_CTPN`),
-  ADD CONSTRAINT `phieunhap_ibfk_2` FOREIGN KEY (`MA_TK`) REFERENCES `taikhoan` (`MA_TK`);
+  ADD CONSTRAINT `phieunhap_ibfk_1` FOREIGN KEY (`MA_TK`) REFERENCES `taikhoan` (`MA_TK`);
 
 --
 -- Constraints for table `quyen`
