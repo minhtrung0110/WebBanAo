4d241215dd6a36d937ae3610411c4024bb9606f9
dekhi1306 <huynhloctruong@gmail.com>
2021-05-09 20:19:38 +0700

    Add pagination for search

--- giaodien/search.php
+++ giaodien/search.php
@@ -1,78 +1,78 @@
+<style>
+html{
+	scroll-behavior: smooth;
+}
+.link{
+	padding: 10px 15px;
+	background: transparent;
+	border:#bccfd8 1px solid;
+	border-left:0px;
+	cursor:pointer;
+	color:#607d8b
+}
+.disabled{
+	cursor:not-allowed;
+	color: #bccfd8;
+}
+.current{
+	background: #bccfd8;
+}
+.first{
+	border-left:#bccfd8 1px solid;
+}
+#pagination{
+	margin-top: 20px;
+
+	padding-top: 30px;
+	border-top: #F0F0F0 1px solid;
+}
+#pagination a{
+	text-decoration:none;
+}
+#paginationWrapper{
+	width:100%;
+	text-align:center
+}
+.dotSign{
+	padding:10px 13px;
+	background:none;
+	border-right: #bccfd8 1px solid;
+}
+</style>
 <?php
-    //Lấy tất cả sản phẩm
 	if(isset($_POST['search_submit']) ){
         $tukhoa=$_POST['search_product']; 
-		$query1=" SELECT * from sanpham WHERE TEN_SP LIKE '%$tukhoa%' GROUP BY TEN_SP ORDER BY DON_GIA ASC";
         $query2=" SELECT * from sanpham WHERE TEN_SP LIKE '%$tukhoa%' GROUP BY TEN_SP ORDER BY DON_GIA ASC";
     }
-    $getAllProduct=mysqli_query($connect,$query1);
     $img="";$title_result="";
     if(mysqli_fetch_array(mysqli_query($connect,$query2))==null) { $img="./images/no-result.png";$title_result="KHÔNG TÌM THẤY SẢN PHẨM  ";}
 
    
 ?>
-	<!-- top Products -->
+<script>
+function getresult(url) {
+	$.ajax({
+		url: url,
+		type: "GET",
+		data:  {rowcount:$("#rowcount").val()},
+		success: function(data){
+			$("#pagination-result").html(data);
+		}        
+   });
+}
+</script>
 <div class="ads-grid py-sm-5 py-4 all-product ">
-            <div class="title_search ">
-                <span class="keyword_search">TÌM KIẾM THEO TỪ KHOÁ:  <strong><?php echo $tukhoa ?></strong></span>
+        <div class="title_search ">
+            <span class="keyword_search">TÌM KIẾM THEO TỪ KHOÁ:  <strong><?php echo $tukhoa ?></strong></span>
+        </div>
+        <h1 class="no_result_search" ><?php echo $title_result ?> </h1>
+        <img class="no_result_search_img"  style="width: 50%;margin: 0 25%;"   src="<?php echo$img?>">
+        <div class="row">
+        	<script>
+				getresult("giaodien/getresult_search.php<?php echo "?search_product=$tukhoa" ?>");
+			</script>
+            <div id="pagination-result" class="col-md-12 col-sm-12 row product ">
+                <input type="hidden" name="rowcount" id="rowcount" />
             </div>
-            <h1 class="no_result_search" ><?php echo $title_result ?> </h1>
-            <img class="no_result_search_img"  style="width: 50%;margin: 0 25%;"   src="<?php echo$img?>">
-			<div class="row">
-				<!-- product left -->
-				<div class="col-md-12 col-sm-12 row product ">
-                    <?php
-                        while($row_all_product=mysqli_fetch_array($getAllProduct)) {
-							$MA_SP=$row_all_product['MA_SP'];
-							$nameproduct=$row_all_product['TEN_SP'];
-                            $price=$row_all_product['DON_GIA'];
-                            $url=$row_all_product['HINH_ANH_URL'];
-								//Kiem tra cac san pham co giam gia hay ko
-							$getSale=mysqli_fetch_assoc(mysqli_query($connect,"SELECT * FROM `chitietgiamgia` WHERE MA_SP='$MA_SP'"));
-							$idsale="";$notificationfoot="";$notificationhead="";$notificationpercent="";
-							if($getSale!=null){
-								$getpercentSale=mysqli_fetch_assoc(mysqli_query($connect,"SELECT PHAN_TRAM_GIAM_GIA FROM chuongtrinhgiamgia WHERE MA_CTGG= '".$getSale['MA_CTGG']."' "));
-								$price=$price - $price* $getpercentSale['PHAN_TRAM_GIAM_GIA'];
-								$idsale=$getSale['MA_CTGG'];
-								//Hien BadGe thong bao % giam gia
-								$notificationhead= '<span class="badge badge-pill badge-danger" 
-								style="font-size: 1.3em;font-weight: bold; float:left; margin-top:6px;margin-left:5px ">-';
-								 $notificationfoot='%</span> ';
-								 $notificationpercent=$getpercentSale['PHAN_TRAM_GIAM_GIA']*100;
-
-							}
-                            
-                    ?>
-                    <div class="col-md-4 col-sm-12 text-center product-content ">
-                    <div class="  product-about">
-						<?php  echo $notificationhead; echo $notificationpercent; echo $notificationfoot; ?>
-                        <img src="images/product-items/<?php echo $url ?>" class="img-fluid img-top-sold">
-                        <div class="overlay">
-                        <a class="info" href="index.php?quanly=detail&id=<?php echo $row_all_product['MA_SP'] ?>&sale=<?php echo $idsale ?>">Chi Tiết</a>
-                        </div>
-                                                                
-                    </div>
-                    <div class="product-infor">
-                        <?php echo $nameproduct ?>
-                            <p style="margin-bottom: 1ex;">
-							
-                            <b class="price " style="color: red"><?php echo number_format($price) ?> VNĐ</b>
-                        </p>			
-                        <div class=" product-button">
-                            <button type="button" class="btn btn-outline-primary col-md-7 " style="float: left;">Thêm Vào Giỏ Hàng</button>
-                            <button type="button" class="btn btn-outline-warning col-md-4 ml-4" style="float: right;">Mua Ngay</button>
-                        </div>
-                        
-                    </div>	
-			        </div>
-                    <?php
-                        }
-
-                    ?>
-                </div>
-			
-			
-			</div>
-		
-	</div>
-	<!-- //top products -->
+        </div>
+</div>
