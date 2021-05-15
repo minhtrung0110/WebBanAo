efccccce08abc48730e4624f53544951bac05f1a
nhattan141 <82261606+nhattan141@users.noreply.github.com>
2021-05-15 12:54:50 +0700

    manage_user

--- admin/dashboard/action/add_account.php
+++ admin/dashboard/action/add_account.php
@@ -13,6 +13,7 @@ if(isset($_GET) ){
     $addQ="INSERT INTO `taikhoan`(`MA_TK`, `MA_GROUP_QUYEN`, `TEN_DANG_NHAP`, `MAT_KHAU`, `STATUS`, `EMAIL`) 
     VALUES ('$MA_TK','$MA_GQ','$TEN_DN','$MK','$TRANG_THAI','$EMAIL') ";
     mysqli_query($connect,$addQ);
+    
     header("Location:../index.php?manage=accounts");
 
 }
