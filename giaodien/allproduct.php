7a4e2fee820f80679a55a16359be407e3c706a34
dekhi1306 <huynhloctruong@gmail.com>
2021-05-16 07:30:24 +0700

    Add advanced search

--- giaodien/allproduct.php
+++ giaodien/allproduct.php
@@ -70,64 +70,23 @@ function getresult(url) {
 						}
 					?>");
 				</script>
-				<!-- product left -->
-
-				<div id="pagination-result" class="col-md-9 col-sm-12 row product ">
-                			<input type="hidden" name="rowcount" id="rowcount" />
-                		</div>
-     
 				<!-- product right -->
 				<div class="col-md-3 mt-lg-0 mt-4 p-lg-0">
 					<div class="side-bar p-sm-4 p-3">
 						<div class="search-hotel border-bottom py-2">
-							<h3 class="agileits-sear-head mb-3">Search Here..</h3>
-							<form action="#" method="post">
-								<input type="search" placeholder="Product name..." name="search" required="">
-								<input type="submit" value=" ">
-							</form>
+							<h3 class="agileits-sear-head mb-3">TÌM KIẾM SẢN PHẨM</h3>
+								<input type="text" id="name">
+								<button id="submitSearch" onclick="filter_data()">Search</button>
 						</div>
 						<!-- price -->
 						<div class="range border-bottom py-2">
 							<h3 class="agileits-sear-head mb-3">Khoảng Giá</h3>
-							<div class="w3l-range">
-								<ul>
-									<li>
-										<a href="#">Under $1,000</a>
-									</li>
-									<li class="my-1">
-										<a href="#">$1,000 - $5,000</a>
-									</li>
-									<li>
-										<a href="#">$5,000 - $10,000</a>
-									</li>
-									<li class="my-1">
-										<a href="#">$10,000 - $20,000</a>
-									</li>
-									<li>
-										<a href="#">$20,000 $30,000</a>
-									</li>
-									<li class="mt-1">
-										<a href="#">Over $30,000</a>
-									</li>
-								</ul>
-							</div>
+							Từ:
+                            <p><input id="min_price" type="text" /></p>
+                            Đến:
+                            <p><input id="max_price" type="text"  /></p>
 						</div>
-						<!-- //price -->
-						<!-- discounts 
-						<div class="left-side border-bottom py-2">
-							<h3 class="agileits-sear-head mb-3">Giảm Giá</h3>
-							<ul>
-								<li>
-									<input type="checkbox" class="checked">
-									<span class="span">5% or More</span>
-								</li>
-							
-							</ul>
-						</div>
-						 //discounts -->
-						<!-- reviews -->
 					
-						<!-- electronics -->
 						<div class="left-side border-bottom py-2">
 							<h3 class="agileits-sear-head mb-3">Loại Sản Phẩm</h3>
 							<ul>
@@ -135,7 +94,7 @@ function getresult(url) {
                                     while($row_type_product=mysqli_fetch_array($getLoai)){ 
                                 ?>
 								<li>
-									<input type="checkbox" class="checked">
+									<input type="checkbox" class="type" value="<?php echo $row_type_product['LOAI_SP']; ?>" >
 									<span class="span"><?php echo $row_type_product['LOAI_SP']  ?></span>
 								</li>
                                 <?php
@@ -150,7 +109,7 @@ function getresult(url) {
                                     while($row_size_product=mysqli_fetch_array($getSize)){ 
                                 ?>
 								<li>
-									<input type="checkbox" class="checked">
+									<input type="checkbox" class="size" value="<?php echo $row_size_product['KICH_THUOC']; ?>">
 									<span class="span"><?php echo $row_size_product['KICH_THUOC']  ?></span>
 								</li>
                                 <?php
@@ -159,26 +118,67 @@ function getresult(url) {
 							</ul>
 						</div>
 					
-						<!-- arrivals -->
-						<div class="left-side border-bottom py-2">
-							<h3 class="agileits-sear-head mb-3">New Arrivals</h3>
-							<ul>
-								<li>
-									<input type="checkbox" class="checked">
-									<span class="span">Last 30 days</span>
-								</li>
-								<li>
-									<input type="checkbox" class="checked">
-									<span class="span">Last 90 days</span>
-								</li>
-							</ul>
-						</div>
-						<!-- //arrivals -->
-					
 					</div>
 					<!-- //product right -->
 				</div>
+                <!-- product left -->
+				
+				<div id="pagination-result" class="col-md-9 col-sm-12 row product ">
+                			<input type="hidden" name="rowcount" id="rowcount" />
+                		</div>
 			</div>
 		
 	</div>
 	<!-- //top products -->
+<style>
+#loading
+{
+	text-align:center; 
+	background: url('loader.gif') no-repeat center; 
+	height: 150px;
+}
+</style>
+
+<script>
+$(document).ready(function(){
+
+    function filter_data()
+    {
+        $('#pagination-result').html('<div id="loading" style="" ></div>');
+        var action = 'fetch_data';
+		var name = $('#name').val();
+        var minimum_price = $('#min_price').val();
+        var maximum_price = $('#max_price').val();
+        var type = get_filter('type');
+        var size = get_filter('size');
+        $.ajax({
+            url:"giaodien/fetch_data.php<?php 
+						$check=isset($_GET['type']);
+						if($check){
+							$type=$_GET['type'];
+							echo "?type=$type";
+						}
+					?>",
+            method:"POST",
+            data:{action:action, name:name, minimum_price:minimum_price, maximum_price:maximum_price, typePro:type, size:size},
+            success:function(data){
+                $('#pagination-result').html(data);
+            }
+        });
+    }
+
+    function get_filter(class_name)
+    {
+        var filter = [];
+        $('.'+class_name+':checked').each(function(){
+            filter.push($(this).val());
+        });
+        return filter;
+    }
+
+    $('#submitSearch').click(function(){
+        filter_data();
+    });
+
+});
+</script>
--- giaodien/deliveryInfor.php
+++ giaodien/deliveryInfor.php
@@ -1,4 +1,5 @@
 <?php 
+	$checkPay=0;
 	$connect =new mysqli("localhost","root","","doanweb2");
 	$connect -> set_charset("utf8");
 	require_once("dbcontroller.php");
@@ -140,6 +141,6 @@
 		document.getElementById("txPhone_delInfor").value=phone;
 	}
 	loadInfor();
-	if(<?php if(isset($checkPay)) echo $checkPay; else echo false?>)
+	if(<?php echo "$checkPay"?>)
 		alert("Cảm ơn quý khách đã mua hàng");
 </script>
